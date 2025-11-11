cc562c828f6b33899add985e81715b29e6b74df6
Data Salaryman <datasalaryman@protonmail.com>
2025-11-11 19:20:40 +0800

    fix(gill/gill-react-vite-tailwind-dirty): add walletui and walletuigill providers

--- gill/gill-react-vite-tailwind-dirty/src/main.tsx
+++ gill/gill-react-vite-tailwind-dirty/src/main.tsx
@@ -1,7 +1,19 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { StrictMode } from 'react'
+import { createSolanaDevnet, createSolanaLocalnet, createWalletUiConfig, WalletUi, WalletUiGillProvider } from '@wallet-ui/react-gill'
+
+const config = createWalletUiConfig({
+  clusters: [createSolanaDevnet(), createSolanaLocalnet()],
+})
 
 createRoot(document.getElementById('root')!).render(
-  <App />
+  <StrictMode>
+    <WalletUi config={config}>
+      <WalletUiGillProvider>
+        <App />
+      </WalletUiGillProvider>
+    </WalletUi>
+  </StrictMode>
 )
